8c0db85e6a7a12d50539629ab378823177f92495
Saurabh Singh <sureshsingh09120@gmail.com>
2026-02-13 21:28:49 +0530

    create main page

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Grid from "@/components/Grid";
+
+export default function Home() {
+    return (
+        <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-slate-50 to-slate-100">
+            <Grid />
+        </main>
+    );
+}
